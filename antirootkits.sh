f89bb9248b178d07e9493f6e7fb08e3e2c3228cd
Leandro Santos <161133257+leandroibov@users.noreply.github.com>
2024-08-15 14:18:52 +0000

    Add files via upload

--- antirootkits.sh
+++ antirootkits.sh
@@ -2,24 +2,23 @@
 sudo echo "";
 sudo echo "Scanning COMBO RKhunter + Chkrootkit";
 sudo echo "Para gerar relatórios use sudo antirootkits >> /home/antirootkits.txt;";
-sudo echo "ATENÇÃO: sudo cat /var/log/rkhunter.log >> /home/antirootkits.txt; no fim do script gravará o arquivo na pasta home chamda antirootkits.txt"
+sudo echo > /home/antirootkits.txt;
 sudo echo "";
-echo "" >> /home/antirootkits.txt;
-echo "###########################################################" >> /home/antirootkits.txt;
-date >> /home/antirootkits.txt;
-echo "###########################################################" >> /home/antirootkits.txt;
 sudo echo "ATUALIZANDO RKHUNTER COM sudo rkhunter --propupd;";
 sudo rkhunter --propupd;
 sudo echo "";
 
 sudo echo "";
+echo;
 sudo echo "RKhunter ACIONANDO - CHECANDO ROOTKITS NO SISTEMA - sudo rkhunter --checkall";
 sudo rkhunter --checkall --sk;
 sudo echo "";
 echo;
+echo;
 sudo echo "APENAS OS WARNINGS DO RKHUNTER - sudo rkhunter -c --enable all --disable none --rwo";
 sudo rkhunter -c --enable all --disable none --rwo;
 sudo echo;
+echo;
 echo;
 echo "CHKROOTKIT comando padrão ---> sudo chkrootkit";
 sudo chkrootkit;
@@ -35,12 +34,9 @@ sudo echo;
 sudo echo;
 sudo echo "CHKROOTKIT - Quiet Mode, mostra só os ALERTAS E SUSPEITAS ---> sudo chkrootkit -q";
 sudo chkrootkit -q;
-echo "" >> /home/antirootkits.txt;
 sudo echo;
 sudo echo;
 sudo echo "Abrindo relatórios";
-#Para o cron somente que gravará na pasta /home/antirootkits.txt
-sudo cat /var/log/rkhunter.log >> /home/antirootkits.txt;
 #Para uso normal do usuário quando executar o script
 sudo gedit /var/log/rkhunter.log;
 sudo gedit /home/antirootkits.txt;
